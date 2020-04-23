57fb0c75b6d9a9ee16ed39b06fc165925e06e11b
BuildTools <justinkrasner98@gmail.com>
2020-04-23 19:21:46 -0400

    Updates

---
+++ precache-manifest.c80bc2a2379e911fd30abe8392e6e266.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d6354e2a8cffcc646a5c7fa1e944bd5",
+    "url": "/currencyconverter/index.html"
+  },
+  {
+    "revision": "7266bbc75892e775ba0d",
+    "url": "/currencyconverter/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "cecc293963442a54507a",
+    "url": "/currencyconverter/static/js/2.4f6cf06b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/currencyconverter/static/js/2.4f6cf06b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7266bbc75892e775ba0d",
+    "url": "/currencyconverter/static/js/main.db6b86a2.chunk.js"
+  },
+  {
+    "revision": "5be96c26a1a2140fb1e4",
+    "url": "/currencyconverter/static/js/runtime-main.02ec697c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/currencyconverter/static/media/logo.5d5d9eef.svg"
+  }
+]);
